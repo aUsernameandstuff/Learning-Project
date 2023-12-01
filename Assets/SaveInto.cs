db4b17e64687fd00c708133a7f2d3e26943ebe50
aUsernameandstuff <145045279+aUsernameandstuff@users.noreply.github.com>
2023-12-01 10:53:07 -0500

    b

---
+++ Assets/SaveInto.cs
@@ -0,0 +1,16 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveInto : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void dwrtf7crhfaeolGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Update is called once per frame
+    
+}
